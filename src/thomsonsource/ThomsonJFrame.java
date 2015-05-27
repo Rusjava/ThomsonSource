1947e496c5c5d55dffea9d3c65718d7fc868d66d
вапрол <@192.168.10.10>
2015-05-27 17:18:35 +0400

    Updated version numbers

--- src/thomsonsource/ThomsonJFrame.java
+++ src/thomsonsource/ThomsonJFrame.java
@@ -60,7 +60,7 @@ import shadowfileconverter.ShadowFiles;
 /**
  *
  * @author Ruslan Feshchenko
- * @version 1.51
+ * @version 1.6
  */
 public class ThomsonJFrame extends javax.swing.JFrame {
 
@@ -2391,7 +2391,7 @@ public class ThomsonJFrame extends javax.swing.JFrame {
     private void jMenuItemAboutActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItemAboutActionPerformed
         // TODO add your handling code here:
         JOptionPane.showMessageDialog(null,
-                "<html>Thompson source parameter calculation. <br>Version: 1.5 <br>Date: May 2015. <br>Author: Ruslan Feshchenko</html>",
+                "<html>Thompson source parameter calculation. <br>Version: 1.6 <br>Date: May 2015. <br>Author: Ruslan Feshchenko</html>",
                 "About TSource", 1);
     }//GEN-LAST:event_jMenuItemAboutActionPerformed
 
