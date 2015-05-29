47a92775f0c798d267470374f0dea2f0cd770709
Samsung <Samsung@127.0.0.1>
2015-05-29 09:38:35 +0400

    Added polarization dependence to ray generation routine

--- src/thomsonsource/ThompsonSource.java
+++ src/thomsonsource/ThompsonSource.java
@@ -109,6 +109,8 @@ public class ThompsonSource implements Cloneable {
     private LaserPulse lp;
     private ElectronBunch eb;
 
+    private double ksi1, ksi2, ksi3;
+
     @Override
     public Object clone() throws CloneNotSupportedException {
         Object tm = super.clone();
@@ -514,6 +516,7 @@ public class ThompsonSource implements Cloneable {
         Vector r = new BasicVector(new double[]{0.0, 0.0, 0.0});
         Vector n0 = new BasicVector(new double[]{0.0, 1.0, 0.0}), As;
         double prob0, prob, EMax, mult = 2, factor, sum = 0;
+        double p = ksi1 * ksi1 + ksi2 * ksi2 + ksi3 * ksi3;
         EMax = directionEnergy(n, n);
         factor = 64 * Math.max(eb.getxWidth(0.0), lp.getWidth(0.0)) * Math.max(eb.getyWidth(0.0), lp.getWidth(0.0))
                 * Math.max(eb.length, lp.length) * 4 * rayXAnglerange * rayYAnglerange
@@ -554,11 +557,11 @@ public class ThompsonSource implements Cloneable {
         // Calculation of the rotated polarization vector
         n = new BasicVector(new double[]{ray[3], ray[4], ray[5]});
         T = getTransform(n, n0);
-        As = T.multiply(new BasicVector(new double[]{1.0, 0.0, 0.0})).divide(Math.sqrt(2));
+        As = T.multiply(new BasicVector(new double[]{1.0, 0.0, 0.0})).multiply(Math.sqrt(1 - p) / Math.sqrt(2));
         ray[6] = As.get(0);
         ray[7] = As.get(1);
         ray[8] = As.get(2);
-        As = T.multiply(new BasicVector(new double[]{0.0, 0.0, 1.0})).divide(Math.sqrt(2));
+        As = T.multiply(new BasicVector(new double[]{0.0, 0.0, 1.0})).multiply(Math.sqrt(1 + p) / Math.sqrt(2));
         ray[15] = As.get(0);
         ray[16] = As.get(1);
         ray[17] = As.get(2);
@@ -604,5 +607,18 @@ public class ThompsonSource implements Cloneable {
         A = n.outerProduct(n0).subtract(n0.outerProduct(n)).add(I.multiply(innerProduct));
         T = I.subtract(D).multiply(1 - innerProduct).add(A);
         return T;
+    }
+
+    /**
+     * Setting Stocks parameters
+     *
+     * @param ksi1
+     * @param ksi2
+     * @param ksi3
+     */
+    public void setPolarization(double ksi1, double ksi2, double ksi3) {
+        this.ksi1 = ksi1;
+        this.ksi2 = ksi2;
+        this.ksi3 = ksi3;
     }
 }
