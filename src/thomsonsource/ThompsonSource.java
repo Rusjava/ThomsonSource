d13b906133d58f019209f201441424e22afac91f
hhhhh gggggg <hhhhh gggggg@192.168.1.4>
2022-07-24 22:47:52 +0300

    Updated version numbers.

--- src/thomsonsource/ThompsonSource.java
+++ src/thomsonsource/ThompsonSource.java
@@ -35,7 +35,7 @@ import org.la4j.vector.dense.BasicVector;
  * The main class containing all physics of LEXG
  *
  * @author Ruslan Feshchenko
- * @version 2.6
+ * @version 2.61
  */
 public class ThompsonSource implements Cloneable {
 
