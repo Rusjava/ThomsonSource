0475d3eec6bfe376d05e98e37505a3c08792f648
rusl <rusl@10.5.1.51>
2024-08-19 16:27:09 +0300

    Dependence on the collisional angle - finished.

--- src/thomsonsource/NonLinearThomsonSource.java
+++ src/thomsonsource/NonLinearThomsonSource.java
@@ -162,7 +162,7 @@ public final class NonLinearThomsonSource extends AbstractThomsonSource {
         pr = n.innerProduct(v);
         csphi = v.innerProduct(lp.getDirection());
         M = inten / sIntensity * (1 + pr) * (1 - mv) / 4;
-        return ordernumber * (1 + csphi * mv)  * lp.getPhotonEnergy() / (1 - pr * mv + M);
+        return ordernumber * (1 + csphi * mv) * lp.getPhotonEnergy() / (1 - pr * mv + M);
     }
 
     /**
@@ -477,9 +477,10 @@ public final class NonLinearThomsonSource extends AbstractThomsonSource {
      * @return
      */
     private double calculateGamma(Vector n, Vector v, double e, double inten) {
-        double rho, pr, fqratio, coef;
+        double rho, pr, fqratio, coef, csphi;
         pr = n.innerProduct(v);
-        rho = inten / sIntensity * (1 + pr) / 4;
+        csphi = v.innerProduct(lp.getDirection()); // Cosine of the angle between the laser pulse and electron bunch
+        rho = inten / sIntensity / 2;
         fqratio = e / ordernumber / lp.getPhotonEnergy();
 
         coef = 2 - fqratio * (1 - pr);
@@ -487,8 +488,8 @@ public final class NonLinearThomsonSource extends AbstractThomsonSource {
             //Returning zero if the expression under the root is not positive
             return 0;
         } else {
-            return (fqratio * (pr + rho) + 1)
-                    / Math.sqrt(fqratio * (1 + 2 * rho + pr) * coef);
+            return Math.sqrt(fqratio * (rho * (1 + pr) + 1 + csphi)
+                    / (2 * (1 + csphi) * coef));
         }
     }
 
@@ -503,17 +504,18 @@ public final class NonLinearThomsonSource extends AbstractThomsonSource {
      * @return
      */
     private double calculateGammaDerivative(Vector n, Vector v, double e, double inten) {
-        double rho, pr, fqration, coef;
+        double rho, pr, fqratio, coef, csphi;
         pr = n.innerProduct(v);
-        rho = inten / sIntensity * (1 + pr) / 4;
-        fqration = e / ordernumber / lp.getPhotonEnergy();
-        coef = 2 - fqration * (1 - pr);
+        csphi = v.innerProduct(lp.getDirection()); // Cosine of the angle between the laser pulse and electron bunch
+        rho = inten / sIntensity / 2;
+        fqratio = e / ordernumber / lp.getPhotonEnergy();
+        coef = 2 - fqratio * (1 - pr);
         if (coef <= 0) {
             //Returning unit if the expression under the root is not positive
             return 1;
         } else {
-            return Math.sqrt((1 + 2 * rho + pr) * (2 - fqration * (1 - pr)) * fqration)
-                    * (2 - fqration * (1 - pr)) / (fqration * (1 + rho) - 1);
+            return 2 * Math.sqrt((1 + csphi) / (1 + csphi + (1 + pr) * rho))
+                    * (calculateGamma(n, v, e, inten) - fqratio * (1 - pr) / (1 + csphi));
         }
     }
 
